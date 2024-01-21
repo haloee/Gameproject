bc116d03c5c4ad48305198acc37d1cafe49e5903
Mátyás Sztaracsek <matyas.sztaracsek@koerber.com>
2024-01-21 11:42:53 +0100

    cell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
